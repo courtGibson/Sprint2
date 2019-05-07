b4f80496aeb315e4f6f8179bdece1cd6f5e42d57
courtney.gibson <courtney.gibson@cs-gecko>
2019-05-07 01:20:26 -0400

    edit

--- planner_networking/src/main/java/fx/compare/CompareViewController.java
+++ planner_networking/src/main/java/fx/compare/CompareViewController.java
@@ -1,13 +1,28 @@
 package fx.compare;
 
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import fx.checkSave.CheckSaveController;
+import fx.homePageView.HomePageViewController;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import loginView.LoginViewController;
 import software_masters.planner_networking.Client;
 import software_masters.planner_networking.Language;
+import software_masters.planner_networking.Main;
 import software_masters.planner_networking.PlanFile;
+import software_masters.planner_networking.PlanNode;
 
 public class CompareViewController 
 {
@@ -84,6 +99,176 @@ public class CompareViewController
 		comparePlans.setText(l.getNewWord("comparePlan.text"));
 
 	
+	}
+	
+	public void logout() throws IOException
+	{
+		System.out.println("logout");
+
+		FXMLLoader loader = new FXMLLoader();
+		Locale locale = Locale.forLanguageTag(langTag);
+		
+		ResourceBundle labels = ResourceBundle.getBundle(propBund, locale);
+
+		loader.setLocation(Main.class.getResource("/loginView/loginView.fxml"));
+		// this.mainView = loader.load();
+		loader.setResources(labels);
+
+		BorderPane newMain = loader.load();
+
+		LoginViewController cont = loader.getController();
+		cont.setLangTag(langTag);
+		cont.setPropBund(propBund);
+		cont.setLanguage(l);
+
+		cont.setMainView(newMain);
+		cont.setTestClient(testClient);
+		cont.setPrimaryStage(primaryStage);
+
+		primaryStage.setUserData(cont);
+		primaryStage.getScene().setRoot(newMain);
+
+	}
+
+	public void homepage() throws IOException
+	{
+		System.out.println("homepage");
+
+
+			FXMLLoader loader = new FXMLLoader();
+			Locale locale = Locale.forLanguageTag(langTag);
+			
+			ResourceBundle labels = ResourceBundle.getBundle(propBund, locale);
+
+			loader.setLocation(Main.class.getResource("/fx/homePageView/homePageView.fxml"));
+			loader.setResources(labels);
+
+			this.mainView = loader.load();
+
+			HomePageViewController cont = loader.getController();
+			cont.setLangTag(langTag);
+			cont.setPropBund(propBund);
+			cont.setLanguage(l);
+
+			cont.setTestClient(testClient);
+			cont.setPrimaryStage(primaryStage);
+			this.testClient = cont.getTestClient();
+
+			cont.setDept(dept.getText());
+			cont.setUser(user.getText());
+
+			primaryStage.getScene().setRoot(mainView);
+
+	}
+	
+	PlanNode currentNode;
+	Boolean builtTree = false;
+	
+	@FXML
+	public TreeView<PlanNode> tree1;
+	
+	@FXML
+	public TreeView<PlanNode> tree2;
+	
+	@FXML
+	public Label nodeLabel;
+	
+	@FXML
+	TextArea contentsArea;
+	
+	public void buildTree() throws RemoteException
+	{
+
+		if (!builtTree)
+		{
+
+
+
+			TreeItem<PlanNode> theRoot = makeTree();
+
+			tree1.setRoot(theRoot);
+
+			tree1.getSelectionModel().selectedItemProperty()
+					.addListener((observable, oldValue, newValue) -> handleTreeClick((TreeItem<PlanNode>) newValue));
+
+			builtTree = true;
+
+		}
+	}
+
+	private void handleTreeClick(TreeItem<PlanNode> newValue)
+	{
+		try
+		{
+
+
+
+			this.currentNode = newValue.getValue();
+
+			nodeLabel.setText(currentNode.getName());
+
+			setContents(currentNode.getData());
+			System.out.println("\n\ncurrentNode data: "+currentNode.getData());
+			
+
+		} catch (Exception E)
+		{
+
+		}
+
+	}
+	
+	public void setContents(String stringContent)
+	{
+
+		contentsArea.setText(stringContent);
+
+	}
+
+	public TreeItem<PlanNode> makeTree() throws RemoteException
+	{
+
+		TreeItem<PlanNode> rootItem = getProducts(testClient.getCurrPlanFile().getPlan().getRoot());
+
+		return rootItem;
+
+	}
+
+	// This method creates an ArrayList of TreeItems (Products)
+	public TreeItem<PlanNode> getProducts(PlanNode root) throws RemoteException
+	{
+
+		// This will be the final ArrayList passed back to FXTreeView.java for build
+		// (should only hold Mission for centre)
+		// ArrayList<TreeItem<PlanNode>> FinalNodeList = new
+		// ArrayList<TreeItem<PlanNode>>();
+
+		TreeItem<PlanNode> currentTreeItem = new TreeItem<PlanNode>(root);
+
+		// FinalNodeList.add(currentTreeItem);
+
+		getKids(root, currentTreeItem);
+
+		return currentTreeItem;
+	}
+
+	private void getKids(PlanNode parentNode, TreeItem<PlanNode> parentTreeItem)
+	{
+
+		if (parentNode.getChildren().isEmpty())
+		{
+			return;
+		}
+
+		for (int i = 0; i < parentNode.getChildren().size(); i++)
+		{
+			TreeItem<PlanNode> newChild = new TreeItem<PlanNode>(parentNode.getChildren().get(i));
+
+			parentTreeItem.getChildren().add(newChild);
+			getKids(parentNode.getChildren().get(i), newChild);
+
+		}
+
 	}
 
 }
